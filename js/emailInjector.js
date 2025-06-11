17ad9d8e5bdd2311d9ec4c965a652621ad898a71
Ryan Young <youngre511@gmail.com>
2025-06-11 18:45:52 -0400

    first commit

---
+++ js/emailInjector.js
@@ -0,0 +1,8 @@
+const e = "support";
+const d = "molesweeper.fun";
+document.getElementById(
+    "em-ad-1"
+).innerHTML = `<a href="mailto:${e}@${d}">${e}@${d}</a>`;
+document.getElementById(
+    "em-ad-2"
+).innerHTML = `<a href="mailto:${e}@${d}">${e}@${d}</a>`;
